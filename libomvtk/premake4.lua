ab83853752abe64750fced4bbd19522e5fea72f6
Vinzenz 'evilissimo' Feenstra <evilissimo@gmail.com>
2009-10-10 13:14:16 +0200

    premake update

--- libomvtk/premake4.lua
+++ libomvtk/premake4.lua
@@ -1,13 +1,55 @@
-all_includes = {
-	"../3rdparty/uuid",
-	"../3rdparty/logging",
-	"../3rdparty/uripp/src",
-	"../3rdparty/tinyxml",
-	"../3rdparty/tinyjson",
-	"../3rdparty/utf8_cpp",
-	"../3rdparty/libnetpp",
-	"../3rdparty/tut-framework",
-}
+function standard_configuration()
+		includedirs  {
+			"../3rdparty/uuid",
+			"../3rdparty/logging",
+			"../3rdparty/uripp/src",
+			"../3rdparty/tinyxml",
+			"../3rdparty/tinyjson",
+			"../3rdparty/utf8_cpp",
+			"../3rdparty/libnetpp",
+			"../3rdparty/tut-framework",
+		}
+
+		--- Define LIBOMVTK_WINDOWS 
+		configuration { "windows" }
+			defines { 
+				"WIN32", 
+				"LIBOMVTK_WINDOWS", 
+				"_WIN32_WINNT=0x0501",
+			}
+
+		--- Define LIBOMVTK_POSIX not on windows
+		configuration { "not windows" }
+			defines { "LIBOMVTK_POSIX" }
+
+		--- Define LIBOMVTK_LINUX on linux
+		configuration { "linux" }
+			defines { "LIBOMVTK_LINUX" }
+
+		configuration "vs*"
+		--- We always want a static runtime for VS for now
+			flags { "StaticRuntime" }
+		--- We always want symbols for VC
+			flags { "Symbols" }
+		--- Disable annoying warnings we know what we do (At least we're supposed to :P)
+			defines { 
+				"_CRT_SECURE_NO_WARNINGS",
+				"_SCL_SECURE_NO_WARNINGS"
+			}
+
+		--- Set g++ options
+		configuration { "gmake" }
+			buildoptions { "-ansi", "-pedantic", "-W -Wall -Wno-long-long", "-std=c++98" }
+
+		--- Set defines for Debug and Release
+		--- For Debug enable symbols generation all compilers
+		configuration "Debug"
+			defines { "_DEBUG" }
+			flags { "Symbols" }
+
+		configuration "Release"
+			defines { "NDEBUG" }
+end
 
 solution "omvtk"
 	configurations { "Debug", "Release" }
@@ -17,56 +59,50 @@ solution "omvtk"
 		language "C++"
 		basedir "libomvtk"
 		targetdir "out/lib"
-		includedirs(all_includes)
 		files { "libomvtk/**.h", "libomvtk/**.cpp" }
-		
-		configuration "windows"
-			flags { "StaticRuntime" }
-			defines { "WIN32", "LIVOMVTK_WINDOWS" }
 
-		configuration "vs*"
-		--- We always want symbols for VC
-			flags { "Symbols" }
-
-		configuration { "gmake" }
-			buildoptions { "-ansi", "-pedantic", "-W -Wall -Wno-long-long", "-std=c++98" }
-
-		configuration "Debug"
-			defines { "_DEBUG" }
-			flags { "Symbols" }
-
-		configuration "Release"
-			defines { "NDEBUG" }
+		--- This needs to be called before any "configuration" call
+		standard_configuration()
 
 	project "omvtk_unittest"
 		kind "ConsoleApp"
 		language "C++"
 		basedir "unit_tests"
 		targetdir "out/bin"
-		links { "omvtk"} --- , "boost_filesystem-mt",  "boost_system-mt",  "boost_thread-mt"}
+		links { "omvtk"} --- , }
 		libdirs { "../3rdparty/lib" }
-		includedirs(all_includes)
-
 		files { "unit_tests/**.h", "unit_tests/**.cpp" }
-		
-		configuration "windows"
-			flags { "StaticRuntime" }
-			defines { "WIN32", "LIVOMVTK_WINDOWS" }
 
-		configuration "vs*"
-		--- We always want symbols for VC
-			flags { "Symbols" }
+		--- This needs to be called before any "configuration" call
+		standard_configuration()
 
-		configuration { "gmake" }
-			buildoptions { "-ansi", "-pedantic", "-W -Wall -Wno-long-long", "-std=c++98" }
+		--- Link OpenSSL for Visual Studio
+		configuration { "vs*", "Debug" }
+			links { "libeay32MTd", "ssleay32MTd" }
 
+		configuration { "vs*", "Release" }
+			links { "libeay32MT", "ssleay32MT" }
+			
+		--- Link OpenSSL for mingw
+		configuration { "windows", "gmake" }
+			links { "libeay32.a", "ssleay32.a" }
+
+		--- link SSL Library and boost on linux
+		--- VC auto links the libs
+		configuration { "linux" }
+			links { 
+				"ssl", 
+				"boost_filesystem-mt",  
+				"boost_system-mt",  
+				"boost_thread-mt",
+				"boost_regex-mt"
+			}
+
+		--- Link uripp and TinyXML++
 		configuration "Debug"
-			defines { "_DEBUG" }
-			flags { "Symbols" }
 			links { "urippd", "ticppd" }
 
 		configuration "Release"
-			links { "uripp", "ticpp" }
 			defines { "NDEBUG" }
 
 			
